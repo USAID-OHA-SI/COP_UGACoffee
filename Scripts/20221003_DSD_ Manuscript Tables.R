79542c4d8cdab42690b5de3e482c6962f3dd06c8
karishmas26 <karishma.srikanth26@gmail.com>
2023-03-27 13:55:41 -0400

    update for JIAS manuscript revisions

--- Scripts/20221003_DSD_ Manuscript Tables.R
+++ Scripts/20221003_DSD_ Manuscript Tables.R
@@ -1,42 +1,31 @@
 #LIBRARIES ---------------------------------------
 
 library(tidyverse)
-library(glamr)
-library(glitr)
-library(gophr)
-library(extrafont)
-library(scales)
-library(tidytext)
-library(glue)
-library(janitor)
-library(ggtext)
-library(extrafont)
-library(waffle)
-library(patchwork)
-library(readxl)
-library(readr)
-library(gt)
+# library(glamr)
+# library(glitr)
+# library(gophr)
+# library(extrafont)
+# library(scales)
+# library(tidytext)
+# library(glue)
+# library(janitor)
+# library(ggtext)
+# library(extrafont)
+# library(waffle)
+# library(patchwork)
+# library(readxl)
+# library(readr)
+# library(gt)
 library(table1)
-library(furniture)
+#library(furniture)
 
-
-table1
 
 
 # IMPORT -----------------------------------------------
 
-#KRS
+
 df <- read_csv("Dataout/20220223-DSD-preference-clean.csv")
 
-
-si_path() %>% 
-  return_latest("20220921-DSD-preference-clean") %>% 
-  read_msd()
-
-df <- readr::read_csv("Dataout/20220921-DSD-preference-clean.csv")
-
-df <- read_csv("Uganda/Dataout/20220921-DSD-preference-clean.csv")
-View(df)
 
 # MAKE TABLES ------------------------------------------
 
@@ -59,7 +48,7 @@ df$is_current_mode_preferred <-
 # ADd labels 
  label(df$vls)        <- "Viral load Suppression"
  label(df$gender)          <- "Gender"
- label(df$agecoarse)          <- "Agecoarse"
+ label(df$agecoarse)          <- "Age Category"
  label(df$region)          <- "Region"
  label(df$missed_appt)          <- "Missed Apointments in the last 12 months"
  label(df$current_arv_mode)          <- "Current ARV Mode"
@@ -94,21 +83,29 @@ table1(~ agecoarse + agecoarse + gender + region + missed_appt
 + current_arv_mode + is_current_mode_preferred + vls, data = df, caption = "Table 1: Baseline Cohort Demographics")
 
 
-# df$vls <- 
-#   factor(df$vls, 
-#          labels=c("Not Virally Suppressed", # Reference
-#                   "Virally Suppressed"))
-# 
-# df$missed_appt <- 
-#   factor(df$missed_appt, 
-#          labels=c("No Missed Appontment", # Reference
-#                   "Missed Appointment"))
+#filter out missing values
+df1 <- df %>% 
+  filter(!is.na(vls),
+         !is.na(missed_appt))
 
-#df[df==0] <- NA
+#Table 2 - outcomes by vls
+table1(~ agecoarse + gender  + is_current_mode_preferred + current_arv_mode | vls, data=df1,
+       row_wise = FALSE,
+       footnote = "CCLAD =Community Client-Led ART delivery,
+       CDDP = Community Drug Distribution Points,
+       FBG = Facility Based Group,
+       FBIM = Facility Based Individual Management,
+       FTDR = Fast-Track Drug Refills",
+       caption = "Table 2: Viral Suppression Outcomes by by Demographic and Clinical Characteristics")
 
-#Table 2 and 3 - outcomes
-table1(~ agecoarse + gender  + is_current_mode_preferred + current_arv_mode | vls, data=df, caption = "Table 2: Viral Suppression Outcomes by ...")
-table1(~ agecoarse + gender +  is_current_mode_preferred + current_arv_mode | missed_appt, data=df, caption = "Table 3: Missed Appointment (in the last 12 months) Outcomes by ...")
+#Table 3 - outcomes by missed appts
+table1(~ agecoarse + gender +  is_current_mode_preferred + current_arv_mode | missed_appt, data=df1,
+       footnote = "CCLAD =Community Client-Led ART delivery,
+       CDDP = Community Drug Distribution Points,
+       FBG = Facility Based Group,
+       FBIM = Facility Based Individual Management,
+       FTDR = Fast-Track Drug Refills",
+       caption = "Table 3: Missed Appointment Outcomes (in the last 12 months) by Demographic and Clinical Characteristics")
   
 
 # tab1 <-furniture::table1(data = df, ~ agecoarse + gender +  is_current_mode_preferred + current_arv_mode | ,
